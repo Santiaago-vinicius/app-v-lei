7b3705f42fa32e92409d6a2c410ca3d01f860bb8
Santiaago-vinicius <vcsanti19@gmail.com>
2026-05-29 12:55:12 -0300

    Versão inicial do PWA Arena Vôlei

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'arena-volei-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './icone.png'
+];
+
+// Instala o Service Worker e salva os arquivos no cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Responde com o cache se o celular estiver offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
